9e4d61a5f4e8e2431edfccf345f6327bdeeb5b42
Abhinav Barthwal <134462974+AbhinavBarthwal@users.noreply.github.com>
2026-09-07 07:43:56 +0530

    Initial Commit

---
+++ Lucid2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lucid2.0
+//
+//  Created by Abhinav Barthwal on 07/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lucid2.0/Lucid2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Lucid2_0App.swift
+//  Lucid2.0
+//
+//  Created by Abhinav Barthwal on 07/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Lucid2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
